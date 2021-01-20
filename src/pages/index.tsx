8205eec45ffdab822c2b9f569858ae2ee52960e7
Igor Melo <igorjmelo4@gmail.com>
2021-01-19 23:26:25 -0300

    adding prismic sdk to project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { client } from "@/lib/prismic";
+import Prismic from "prismic-javascript";
+import { Document } from "prismic-javascript/types/documents";
+import PrismicDOM from "prismic-dom";
 
 import { Title } from "../styles/pages/Home";
 import SEO from "@/components/SEO";
 
-interface IProduct {
-  id: string;
-  title: string;
-}
-
 interface HomeProps {
-  recommendedProducts: IProduct[];
+  recommendedProducts: Document[];
 }
 
 export default function Home({ recommendedProducts }: HomeProps) {
@@ -27,7 +27,13 @@ export default function Home({ recommendedProducts }: HomeProps) {
         <ul>
           {recommendedProducts.map((recommendedProduct) => {
             return (
-              <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
+              <li key={recommendedProduct.id}>
+                <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                  <a>
+                    {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                  </a>
+                </Link>
+              </li>
             );
           })}
         </ul>
@@ -37,14 +43,19 @@ export default function Home({ recommendedProducts }: HomeProps) {
 }
 
 export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/recommended`
-  );
-  const recommendedProducts = await response.json();
+  // const response = await fetch(
+  //   `${process.env.NEXT_PUBLIC_API_URL}/recommended`
+  // );
+  // const recommendedProducts = await response.json();
+
+  //PRISMIC
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at("document.type", "product"),
+  ]);
 
   return {
     props: {
-      recommendedProducts,
+      recommendedProducts: recommendedProducts.results,
     },
   };
 };
